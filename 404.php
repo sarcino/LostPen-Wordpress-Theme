95c3e7f4dce601332f3a29d2defc3e675e6e7d79
Sarka Sevcikova <sarka.sevci@gmail.com>
2021-02-06 13:42:10 +0100

    404 page, single page and comments
    Comment form
    404 page copy
    article link style change

--- 404.php
+++ 404.php
@@ -4,9 +4,13 @@
 <article>
 <div class="container-404">
 
-<h2>Ale ne! Tahle stránka tu není</h2>
+<h2>Sorry. Page not found</h2>
 
-<img src="../img/404-kittens.jpg" alt="Světlá koťátka sedící na dětském vozíčku na zahradě" />
+<p>
+Feeling lost? You can always go <a href="<?php echo site_url(''); ?>" name="top">home</a>.   
+</p>
+
+
 
 
 </div>
--- single.php
+++ single.php
@@ -2,80 +2,26 @@
 
 <main>
 
-<?php while (have_posts()) : the_post(); ?>
+<?php while (have_posts()) { the_post(); ?>
 
 <article>
 
 <h2><?php the_title(); ?></h2>
-
 <div class="date"><?php the_time("j. F Y"); ?>  |  <a href="#"><?php echo get_the_tag_list() ?></a></div>
+<img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>">
 
 <?php the_content(); ?>
+
 <div class="comments">
-
-<?php 
-
-$fields = array(
-   
-    'author' =>
-    '<li class="comment-form-author"><label for="author">' . __('Jméno') .
-    ( $req ? '<span class="required">*</span>' : '' ) . '</label> ' .
-    '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
-    '" size="30"' . $aria_req . ' /></li>',
-
-  'email' =>
-    '<li class="comment-form-email"><label for="email">' . __('E-mail') .
-    ( $req ? '<span class="required">*</span>' : '' ) . '</label> ' .
-    '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
-    '" size="30"' . $aria_req . ' /></li>'
-);
-
-
-		
-
-
-$args = array(
-    "fields" => $fields,
-    "comment_field" => ' <li class="textarea comment-form-comment">
-    <label for="comment">Komentář</label>
-    <textarea class="text" data-autoresize rows="2" id="komentar" name="user_comment"></textarea>
-</li>',
-"title_reply"  => __("Komentáře"),
-"label_submit" => __("Okometovat")
-
-);
-
-
-    comment_form($args);
-
-    $comments_number = get_comments_number();
-    if($comments_number != 0) {
-    ?>  
-
-      
-    <ul class="all-comments">
-
-    <?php
-
-    $comments = get_comments(array(
-        "post_id" => $post -> ID,
-        "status" => "approve"
-    ));
-    wp_list_comments(array(
-        "per_page" => 15
-    ), $comments);
-    ?>
-
-    </ul>
-  
-
-<?php } ?>
-
+<?php comment_form(); ?>
 </div>
+
+
+
 </article>
 
-<?php endwhile; ?>
 
+<?php } ?>
 </main>
 
 
