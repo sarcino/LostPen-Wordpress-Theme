8e8b536e1e5df750081eb2f31d075a9b6f43542a
Sarka Sevcikova <sarka.sevci@gmail.com>
2021-02-06 21:52:59 +0100

    EN translation

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form method="get" action="<?php echo home_url() ?>">
     <input type="search" name="s" value="<?php echo get_search_query(); ?>">
-    <input type="submit" id="search-btn" value="Hledej" >
+    <input type="submit" id="search-btn" value="Search" >
 </form>
